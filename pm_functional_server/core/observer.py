c7526c06a5bd1ea611d5dd217bffd971de5bbffd
vlaskatenev <hiperst35@gmail.com>
2020-11-22 22:59:20 +0300

    make observer

---
+++ pm_functional_server/core/observer.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from random import randrange
+from typing import List
+
+
+class Subject(ABC):
+    """
+    Интферфейс издателя объявляет набор методов для управлениями подписчиками.
+    """
+
+    @abstractmethod
+    def attach(self, observer) -> None:
+        """
+        Присоединяет наблюдателя к издателю.
+        """
+        pass
+
+    @abstractmethod
+    def detach(self, observer) -> None:
+        """
+        Отсоединяет наблюдателя от издателя.
+        """
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        """
+        Уведомляет всех наблюдателей о событии.
+        """
+        pass
+
+
+class ConcreteSubject(Subject):
+    """
+    Издатель владеет некоторым важным состоянием и оповещает наблюдателей о его
+    изменениях.
+    """
+
+    _state: int = None
+    """
+    Для удобства в этой переменной хранится состояние Издателя, необходимое всем
+    подписчикам.
+    """
+
+    _observers: List = []
+    """
+    Список подписчиков. В реальной жизни список подписчиков может храниться в
+    более подробном виде (классифицируется по типу события и т.д.)
+    """
+
+    def attach(self, observer) -> None:
+        self._observers.append(observer)
+        print("Subject: Attached an observer.", self._observers)
+
+    # здесь будет удаление подписки на объект и дальнейшая запись в БД об успешном окончании задания
+    def detach(self, observer) -> None:
+
+        self._observers.remove(observer)
+        print("Subject: Detached an observer.", self._observers)
+    """
+    Методы управления подпиской.
+    """
+
+    def notify(self, response_from_pc) -> None:
+        """
+        Запуск обновления в каждом подписчике перед отпиской.
+        """
+
+        print("Subject: Notifying observers...")
+        for observer in self._observers:
+            if response_from_pc['idInstall'] == observer['idInstall']:
+                observer['resultWork'] = response_from_pc['resultWork']
+            print('Это комп на который я подписан: ', observer)
+            
+
+
--- pm_functional_server/rest_api/views.py
+++ pm_functional_server/rest_api/views.py
@@ -7,19 +7,52 @@ from rest_framework import generics
 from .serializers import ResultWorkDetailSerializer
 from .tasks import get_data_for_task_manager
 from celery.result import AsyncResult
-
+from django.conf import settings
 
 # method for client PC and WEB main server
 # example json for post method
 # {
-#     "date": "2020-11-5T21:35:48.118055Z",
 #     "id_install": 255777,
 #     "result_work": false
 # }
 class InsertWorkData(generics.CreateAPIView):
+    """Добавляем задание в слушателя и записываем его в БД"""
     permission_classes = (IsAuthenticated,)
 
     serializer_class = ResultWorkDetailSerializer
+
+    def post(self, request):
+        task = get_data_for_task_manager.delay(request.data)
+        ResultWorkDetailSerializer.objects.create(id_install=request.data["id_install"], result_work=request.data["result_work"])
+
+        settings.OBSERVER.attach(request.data)
+
+        return JsonResponse({"task_id": task.id}, status=202)
+
+
+# {
+#     "id_install": 255777,
+#     "result_work": True
+# }
+class InsertWorkDataFromClient(generics.CreateAPIView):
+    """Клиент уведомляет слушателя, вносим запись в БД, изменяем состояние слушателя и удаляем объект клиента из массива слушателя при занчении resultWork = True"""
+    permission_classes = (IsAuthenticated,)
+
+    serializer_class = ResultWorkDetailSerializer
+
+    def post(self, request):
+        ResultWorkDetailSerializer.objects.create(id_install=request.data["id_install"], result_work=request.data["result_work"])
+        
+        if request.data["resultWork"]:
+            settings.OBSERVER.notify(request.data)
+            settings.OBSERVER.detach(request.data)
+
+        return JsonResponse({"task": request.data}, status=200)
+
+
+
+
+
 
 
 # method ONLY web main server
