510cb1b59f6cc55ad1e4af8af560d012b0becacb
vlaskatenev <hiperst35@gmail.com>
2021-01-04 22:00:41 +0300

    deleted scripts and methods for work mysql

--- pm_main_server/data_construction/views.py
+++ pm_main_server/data_construction/views.py
@@ -4,13 +4,9 @@ from celery.result import AsyncResult
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from data_construction.for_views.Global.pure_functions_global_api import create_context_log
-from data_construction.for_views.HistoryDetail.pure_functions_historydetail import create_object_history_detail
 from data_construction.for_views.StartInstall.function_start_install import request_to_start_install
 from data_construction.for_views.pure_functions_history import updateDict, to_start_end_day
-from data_construction.for_views.Manually.manually_pure_functions import create_object_to_choose_programm
 from services_main_server.ldap import find_computer_in_ad, create_ps1_script_for_client
-# import data_construction.for_views.pure_functions_runningprocess
 from data_construction.models import LogsInstallationSoft, Soft
 from django.utils import timezone
 
@@ -34,11 +30,13 @@ class History(APIView):
 
 
 # Детальная информация об одной установке
+# {
+#     "data": 255853
+# }
 class HistoryDetail(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        # return Response(create_object_history_detail(request.data['data']))
         all_data = LogsInstallationSoft.objects.filter(startnumber=request.data['data']).values(
                 'date_time',
                 'computer_name', 
@@ -80,7 +78,6 @@ class StartInstall(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-
         # записываем событие в таблицу LogsInstallationSoft
         id_install_array = []
         for comp_name in request.data["computer_name"]:
